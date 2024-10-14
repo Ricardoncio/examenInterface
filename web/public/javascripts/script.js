6734fa0891557a96229f533bcd0353387138dd02
Ricardoncio <rsolo719@gmail.com>
2024-10-14 08:59:21 +0200

    Examen

---
+++ web/public/javascripts/script.js
@@ -0,0 +1,9 @@
+document.getElementById("loginBtn").addEventListener('click', () => {
+    document.getElementById("off-canvas").classList.remove("hide");
+
+})
+
+document.getElementById("closeBtn").addEventListener('click', () => {
+    document.getElementById("off-canvas").classList.add("hide");
+
+})
